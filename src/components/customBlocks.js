25dd32f496cbf104941613259acef594bea4491d
Quinn Barber <qbarber2020@gmail.com>
2023-02-22 15:21:07 -0500

    Forward or backward, and know its ms

--- src/components/customBlocks.js
+++ src/components/customBlocks.js
@@ -2,7 +2,7 @@ import * as Blockly from 'blockly/core';
 
 let reactTurnField = {
     "type": "turn",
-    "message0": "turn: %1",
+    "message0": "turn: %1 ms",
     "args0": [
         {
             "type": "field_number",
@@ -11,19 +11,31 @@ let reactTurnField = {
             "precision": 1
         }
     ],
+    "message1": "forward: %1",
+    "args1": [
+        {
+            "type": "field_checkbox"
+        }
+    ],
     "previousStatement": null,
     "nextStatement": null,
 };
 
 let reactMoveField = {
     "type": "move",
-    "message0": "move: %1",
+    "message0": "move: %1 ms",
     "args0": [
         {
             "type": "field_number",
             "min": 0,
             "max": 9999,
             "precision": 1
+        }
+    ],
+    "message1": "forward: %1",
+    "args1": [
+        {
+            "type": "field_checkbox"
         }
     ],
     "previousStatement": null,
@@ -34,6 +46,7 @@ Blockly.Blocks['move'] = {
     init: function() {
         this.jsonInit(reactMoveField);
         this.setStyle('loop_blocks');
+        this.setColour(0);
     }
 }
 
@@ -41,5 +54,6 @@ Blockly.Blocks['turn'] = {
     init: function() {
         this.jsonInit(reactTurnField);
         this.setStyle('loop_blocks');
+        this.setColour(80);
     }
 }
